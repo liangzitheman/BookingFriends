93b9330671fc14fcc20589ae85d9169a87b22dca
zhangliang <ZhangLiang@huaqin.com@huaqin.com>
2017-08-14 09:34:44 +0800

    first commit

---
+++ app/src/main/java/com/example/ubuntu/bookingfriends/db/MyOpenHelper.java
@@ -0,0 +1,12 @@
+package com.example.ubuntu.bookingfriends.db;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by ubuntu on 17-7-26.
+ */
+
+public class MyOpenHelper extends SQLiteOpenHelper {
+    
+
+}
